13c9a838024e24b35154f2bb50f5413b4697ef43
a.zakharenkov <a.zakharenkov@mkskom.ru>
2026-03-27 01:33:50 +0300

    initial commit

---
+++ src/test/java/ru/zahaand/lifesync/LifesyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zahaand.lifesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifesyncBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

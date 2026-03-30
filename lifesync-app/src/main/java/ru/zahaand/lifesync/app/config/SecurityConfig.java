ff93c07c16cbb1ab2cb95bab38aecf4385cbdbee
a.zakharenkov <a.zakharenkov@mkskom.ru>
2026-03-30 21:08:18 +0300

    docs(api): Дополнить OpenAPI документацию

--- lifesync-app/src/main/java/ru/zahaand/lifesync/app/config/SecurityConfig.java
+++ lifesync-app/src/main/java/ru/zahaand/lifesync/app/config/SecurityConfig.java
@@ -2,13 +2,13 @@ package ru.zahaand.lifesync.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.http.HttpStatus;
 import ru.zahaand.lifesync.domain.user.TokenProvider;
 import ru.zahaand.lifesync.web.user.JwtAuthenticationFilter;
 
@@ -31,7 +31,8 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                         .requestMatchers("/swagger-ui/**", "/swagger-ui.html",
-                                "/v3/api-docs/**", "/v3/api-docs.yaml").permitAll()
+                                "/v3/api-docs/**", "/v3/api-docs.yaml",
+                                "/lifesync-api.yaml").permitAll()
                         .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated())
                 .exceptionHandling(ex -> ex
